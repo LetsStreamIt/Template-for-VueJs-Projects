e44c0e89500fa2dcd4a849dab10087f7d30a624e
Luca Fabri <luca.fabri1999@gmail.com>
2024-08-07 17:44:19 +0200

    docs: add vuepress

---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,3 @@
+import "/home/luca/tmp/template/template-for-vuejs-projects/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "/home/luca/tmp/template/template-for-vuejs-projects/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "/home/luca/tmp/template/template-for-vuejs-projects/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
